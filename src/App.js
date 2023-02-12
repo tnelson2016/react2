5aec38c8e8f108bc74375bae920a5ff76a80912e
tnelson2016 <troy.nelson@bracari.com>
2023-02-12 13:10:56 -0500

    getting ready for css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev')
   const showEmployees = true
   return (
     <div className="App">
        { showEmployees ? (
         <>
-        <Employee />
-        <Employee/>
-        <Employee/>
-        <Employee/>
-        <Employee />
+          <input type='text'
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value)
+            }}
+          />
+        <Employee name = 'Troy' role = 'Intern' />
+        <Employee name = 'Abby' role = {role}/>
+        <Employee name = 'John' />
+      
         </>
       )
           :
---
+++ src/components/Employee.js
@@ -0,0 +1,12 @@
+function Employee(props)
+{
+
+    return (
+        <>
+            <h3>Employee {props.name}</h3>
+            <p>{props.role ? props.role: 'No role'}</p>
+        </>
+    );
+}
+
+export default Employee;
