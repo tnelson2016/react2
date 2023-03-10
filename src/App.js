7e702ec909b0ce27dded8d19f8ebfb3cba964829
tnelson2016 <troy.nelson@bracari.com>
2023-03-09 22:01:04 -0500

    mobile stylings

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ import AddEmployee from './components/AddEmployee'
 import EditEmployee from './components/EditEmployee'
 import Header from './components/Header'
 import Employees from './pages/Employees'
+import Customers from './pages/Customers'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
     return (
-        <Header>
-            <Employees />
-        </Header>
+        <BrowserRouter>
+            <Header>
+                <Routes>
+                    <Route path="/employees" element={<Employees />} />
+                    <Route path="/customers" element={<Customers />} />
+                </Routes>
+            </Header>
+        </BrowserRouter>
     )
 }
 
